aff4c89335808e50185f65e7fc00906d4bf7f522
hana_milad <89672587+hanamilad@users.noreply.github.com>
2024-08-12 00:33:23 +0300

    Update loading.js

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
  const Loading = () => {
   return (
-<div className='h-full absolute top-0 right-0 left-0 mt-[50%]'>
+<div className='h-screen flex items-center justify-center'>
 <svg className="pl m-auto" width="240" height="240" viewBox="0 0 240 240">
 	<circle className="pl__ring pl__ring--a" cx="120" cy="120" r="105" fill="none" stroke="#000" strokeWidth="20" strokeDasharray="0 660" strokeDashoffset="-330" strokeLinecap="round"></circle>
 	<circle className="pl__ring pl__ring--b" cx="120" cy="120" r="35" fill="none" stroke="#000" strokeWidth="20" strokeDasharray="0 220" strokeDashoffset="-110" strokeLinecap="round"></circle>
